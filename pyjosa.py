20ccec8c8317a431547e2acaa37ce125e9473be0
jaru <myevan80@gmail.com>
2012-12-14 01:20:01 +0900

    + 종성 ㄹ 예외 처리 ! 정규표현식 미리 컴파일

--- pyjosa.py
+++ pyjosa.py
@@ -5,16 +5,30 @@ JOSA_PAIRD = {
     u"(이)가" : (u"이", u"가"),
     u"(을)를" : (u"을", u"를"),
     u"(은)는" : (u"은", u"는"),
+    u"(으)로" : (u"으로", u"로"),
 }
 
+JOSA_REGEX = re.compile(u"\(이\)가|\(을\)를|\(은\)는|\(으\)로")
+
 def has_jong(char):
-    code = ord(char) - 0xac00
-    return code % 28
+    char_code = ord(char)
+    if char_code >= 0xac00 and char_code <= 0xD7A3: # 가 ~ 힣까지
+        local_code = char_code - 0xac00 # '가' 이후 로컬 코드
+        jong_code = local_code % 28
+        if jong_code: # 종성이 있는 경우
+            if jong_code == 8: # ㄹ 종성인 경우
+                return False
+            else:
+                return True
+        else: # 종성이 없는 경우
+            return False
+    else: # 한글이 아닐 경우
+        return False
 
 def replace_josa(src):
     tokens = []
     base_index = 0
-    for mo in re.finditer(u"\(이\)가|\(을\)를|\(은\)는", src):
+    for mo in JOSA_REGEX.finditer(src):
         prev_token = src[base_index:mo.start()]
         prev_char = prev_token[-1]
         tokens.append(prev_token)
@@ -35,7 +49,7 @@ if __name__ == '__main__':
         def test(self):
             self.assertEquals(replace_josa(u"아노아(이)가 공격했다"), u"아노아가 공격했다")
             self.assertEquals(replace_josa(u"주펫(이)가 공격했다"), u"주펫이 공격했다")
-            self.assertEquals(replace_josa(u"아노아(은)는 자루(을)를 칭송했다"), u"아노아는 자루를 칭송했다")
+            self.assertEquals(replace_josa(u"아노아(은)는 자루(을)를 칭송하고 절(으)로 들어갔습니다."), u"아노아는 자루를 칭송하고 절로 들어갔습니다.")
 
     unittest.main()
 
